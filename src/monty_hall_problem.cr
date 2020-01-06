e4849228f2174cbc569fa298a66b843dbd0bcbfa
takuya matsunaga <takanotume24@gmail.com>
2020-01-06 14:50:05 +0900

    use hash

--- src/monty_hall_problem.cr
+++ src/monty_hall_problem.cr
@@ -1,39 +1,42 @@
-def select_door(correct_door, first_selected_door, doors) : Array(Int32)
+def openable_doors(correct_door, first_selected_door, doors) : Hash(Int32, Bool)
   doors.delete first_selected_door
 
-  if correct_door == first_selected_door
-    doors.delete_at Random.rand doors.size
-  else
-    deletable_doors = doors.clone
-    deletable_doors.delete correct_door
-    deletable_doors.delete first_selected_door
-    doors.delete deletable_doors[Random.rand deletable_doors.size]
-  end
+  hazure_doors = doors.select { |key, value| value == false }
+  delete_door = hazure_doors.keys[Random.rand hazure_doors.keys.size]
+  doors.delete delete_door
+
   return doors
 end
 
 def choise_second_door(doors) : Int
-  return doors[Random.rand doors.size]
+  return doors.keys[Random.rand doors.keys.size]
 end
 
-loop : Int64 = ARGV[0].to_i64
+door_num = ARGV[0].to_i
+loop : Int64 = ARGV[1].to_i64
 
 atari = 0
 atari_changed = 0
+doors = Hash(Int32, Bool).new
 
 loop.times do |i|
-  correct_door = Random.rand 3
-  first_selected_door = Random.rand 3
-  doors = Array.new(3) { |i| i }
+  correct_door = Random.rand door_num
+  first_selected_door = Random.rand door_num
 
-  if correct_door == first_selected_door
+  door_num.times do |i|
+    doors[i] = false
+  end
+
+  doors[correct_door] = true
+
+  if doors[first_selected_door]
     atari += 1
   end
 
-  doors = select_door correct_door, first_selected_door, doors
+  doors = openable_doors correct_door, first_selected_door, doors
   second_selected_door = choise_second_door doors
 
-  if correct_door == second_selected_door
+  if doors[second_selected_door]
     atari_changed += 1
   end
 end
