a1123528e0bcbe18118fac1aeaa7c23cba24f7ff
takuya matsunaga <takanotume24@gmail.com>
2020-01-20 14:49:09 +0900

    csv output

--- src/monty_hall_problem_para.cr
+++ src/monty_hall_problem_para.cr
@@ -1,5 +1,3 @@
-require "math"
-
 def openable_doors(correct_door, first_selected_door, doors) : Hash(Int32, Bool)
   doors.delete first_selected_door
   hazure_doors = doors.select { |key, value| value == false }
@@ -13,26 +11,18 @@ def choise_second_door(doors : Hash(Int32, Bool)) : Int
 end
 
 door_num = ARGV[0].to_i
-m : Int64 = ARGV[1].to_i64
-loop : Int64 = ARGV[2].to_i64
-
-file = File.open("data_m_#{m}_loop_#{loop}.csv", mode = "w")
+loop : Int64 = ARGV[1].to_i64
 
 atari = 0
 atari_changed = 0
 
 doors = Hash(Int32, Bool).new
-datas = Hash(Float64, Int64).new
 
-m.times do |i|
-  datas[(i / m).significant(Math.log10 m + 1)] = 0
-end
+channel_atari = Channel(Int64).new
+channel_atari_changed = Channel(Int64).new
 
-loop.times do |j|
-  atari_changed = 0
-  atari = 0
-
-  m.times do |i|
+loop.times do |i|
+  spawn do
     correct_door = Random.rand door_num
     first_selected_door = Random.rand door_num
 
@@ -43,25 +33,35 @@ loop.times do |j|
     doors[correct_door] = true
 
     if doors[first_selected_door]
-      atari += 1
+      channel_atari.send 1
+    else
+      channel_atari.send 0
     end
 
     doors = openable_doors correct_door, first_selected_door, doors
     second_selected_door = choise_second_door doors
 
     if doors[second_selected_door]
-      atari_changed += 1
+      channel_atari_changed.send 1
+    else
+      channel_atari_changed.send 0
     end
   end
-  datas[(atari / m).significant(Math.log10 m + 1)] += 1
-  # file << "#{j},#{atari / m},#{atari_changed / m}\n"
 end
 
-datas.each do |k, v|
-  file << "#{k},#{v}\n"
+loop.times do
+  spawn do
+    atari += channel_atari.receive
+  end
 end
+
+loop.times do
+  spawn do
+    atari_changed += channel_atari_changed.receive
+  end
+end
+
+Fiber.yield
 
 puts "変えずに当たる確率 : \t#{atari / loop}"
 puts "変えて当たる確率 : \t#{atari_changed / loop}"
-
-file.close
